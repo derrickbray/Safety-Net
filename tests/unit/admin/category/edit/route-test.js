7fc88ce0589f0d892679cc8a8423a3479ec5b762
“Shaun <shaunwmusic@gmail.com>
2016-11-30 11:34:41 -0600

    Create routes

---
+++ tests/unit/admin/category/edit/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:admin/category/edit', 'Unit | Route | admin/category/edit', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
