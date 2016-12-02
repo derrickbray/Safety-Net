0de2ab1e94aba53550155d731206a59047cd4169
Derrick Bray <derrickbray@yahoo.com>
2016-12-02 11:34:01 -0600

    add user and pledge model

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  orgName: DS.attr('string'),
+  email: DS.attr('string'),
+  password: DS.attr('string'),
+  phone: DS.attr('string'),
+  website: DS.attr('string'),
+  isAdmin: DS.attr('boolean'),
+  isApproved: DS.attr('boolean')
+});
