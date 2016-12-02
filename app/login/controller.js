b0813dd902fa676148ee23509e2fb6ffdffb43f2
Derrick Bray <derrickbray@yahoo.com>
2016-12-02 15:25:28 -0600

    login and signup working?

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
   actions: {
-    checkForm() {
-      this.transitionToRoute('user.pledge');
+    checkForm(formValues) {
+      const secretStuff = {
+        identification: formValues.email,
+        password: formValues.password,
+      };
+      const authenticator = 'authenticator:jwt';
+
+      this.get('session').authenticate(authenticator, secretStuff).then(() => {
+        this.transitionToRoute('user.pledge');
+      });
     },
   }
 });
