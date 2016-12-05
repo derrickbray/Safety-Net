1ed5136004f871696facbdcdf867d44e9849f33e
“Shaun <shaunwmusic@gmail.com>
2016-12-05 16:50:28 -0600

    Add google api key & connect data

--- app/models/organization.js
+++ app/models/organization.js
@@ -7,5 +7,9 @@ export default DS.Model.extend({
   website: DS.attr('string'),
   lat: DS.attr('number'),
   lng: DS.attr('number'),
-  user: DS.belongsTo('user')
+  user: DS.belongsTo('user'),
+
+  location: Ember.computed('lng', 'lat', function() {
+    return [this.get('lat'), this.get('lng')];
+  }),
 });
