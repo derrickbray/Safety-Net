e559a242fe0501970e24b3b642079287c4b9e21f
Derrick Bray <derrickbray@yahoo.com>
2016-12-05 15:14:38 -0600

    ember

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'
-// http://localhost:3333/api/BLAH
+
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:token',
