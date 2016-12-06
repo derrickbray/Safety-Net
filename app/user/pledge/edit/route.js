457bcc302e63e8087abb40b9db5cf0178e8e5cef
Derrick Bray <derrickbray@yahoo.com>
2016-12-06 08:37:57 -0600

    update pledge

--- app/user/pledge/edit/route.js
+++ app/user/pledge/edit/route.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addPledge(formValues) {
+      if (formValues) {
+        const pledge = this.store.createRecord('pledge', formValues);
+        pledge.set('pledge', this.model);
+        console.log(pledge);
+        // debugger;
+        pledge.save()
+        .then(() => {
+        alert('Your pledge was updated!');
+
+          this.transitionToRoute('user.pledge');
+        });
+      } else{
+        alert('Sorry, Something Went Wrong. Try Again.');
+      }
+    },
+  },
 });
