a90825d2739d99f39808fb438532a90990652ac2
“Shaun <shaunwmusic@gmail.com>
2016-12-06 13:39:34 -0600

    Add nashville api and connect data

--- app/resources/controller.js
+++ app/resources/controller.js
@@ -13,15 +13,30 @@ export default Ember.Controller.extend({
   },
   lat: 36.1526986,
   lng: -86.7761604,
-  zoom: 10,
-  emberConfLocation: [36.1526986, -86.7761604],
-  hotel: [36.1526986, -86.8061604],
+  zoom: 15,
+
+  lookupSocrataData() {
+    const northEast = `${this.get('edges._northEast.lng')} ${this.get('edges._northEast.lat')}`;
+    const northWest = `${this.get('edges._southWest.lng')} ${this.get('edges._northEast.lat')}`;
+    const southWest = `${this.get('edges._southWest.lng')} ${this.get('edges._southWest.lat')}`;
+    const southEast = `${this.get('edges._northEast.lng')} ${this.get('edges._southWest.lat')}`;
+    const polygon = `MULTIPOLYGON (((${northEast}, ${northWest}, ${southWest}, ${southEast}, ${northEast})))`;
+
+    fetch(`https://data.nashville.gov/resource/8zc7-2afq.json?$where=within_polygon(location_1, '${polygon}')`)
+      .then(r => r.json())
+      .then((nashvilleResources) => {
+        this.set('nashvilleResources', nashvilleResources);
+      });
+  },
 
   actions: {
     updateCenter(e) {
+      this.set('edges', e.target.getBounds());
       let center = e.target.getCenter();
       this.set('lat', center.lat);
       this.set('lng', center.lng);
+
+      this.lookupSocrataData();
     },
     showDropdown() {
 
