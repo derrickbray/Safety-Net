a1f397c243735f9a3547447de09e8993d8498486
Derrick Bray <derrickbray@yahoo.com>
2016-12-08 11:36:38 -0600

    pledge approve work

--- app/admin/pledge/controller.js
+++ app/admin/pledge/controller.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  currentUser: Ember.inject.service(),
   session: Ember.inject.service(),
+
   actions: {
     toggleAndSave(property) {
       const pledge = this.model;
