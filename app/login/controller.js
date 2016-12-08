0576191cf47e534c5dc531c867ed819a1009bba5
Derrick Bray <derrickbray@yahoo.com>
2016-12-07 18:00:42 -0600

    ermigurd the login auth admin to admin

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,18 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  currentUser: Ember.inject.service(),
   session: Ember.inject.service(),
   actions: {
     checkForm(formValues) {
       const secretStuff = {
         identification: formValues.email,
-        password: formValues.password,
+        password: formValues.password
       };
       const authenticator = 'authenticator:jwt';
 
       this.get('session').authenticate(authenticator, secretStuff).then(() => {
-        this.transitionToRoute('user.pledge');
+
+        return this.get('currentUser').loadCurrentUser();
+      }).then(() => {
+        if (this.get('currentUser.user.isAdmin')) {
+          this.transitionToRoute('admin');
+        } else {
+          this.transitionToRoute('user.pledge');
+        }
       });
-    },
+    }
   }
 });
