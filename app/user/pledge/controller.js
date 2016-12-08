7590cfab7464109e242f45892c30ad675c6f6b31
Derrick Bray <derrickbray@yahoo.com>
2016-12-07 20:46:06 -0600

    try to add isClaimed to the user/pledge template

--- app/user/pledge/controller.js
+++ app/user/pledge/controller.js
@@ -2,4 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   currentUser: Ember.inject.service(),
+  session: Ember.inject.service(),
+  actions: {
+    toggleAndSave(property) {
+      const user = this.model;
+      user.toggleProperty(property);
+      user.save();
+    },
 });
