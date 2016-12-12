3676ef57e110fddf770a21c71ad5270766fbc239
Derrick Bray <derrickbray@yahoo.com>
2016-12-12 13:38:24 -0600

    stuff

--- app/models/category.js
+++ app/models/category.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   importedFromGov: DS.attr('boolean'),
-  organization: DS.hasMany('organization')
+  organizations: DS.hasMany('organization')
 });
