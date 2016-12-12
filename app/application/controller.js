50e761b5b03b701b7e0c9717bae73e2b5adf7b81
“Shaun <shaunwmusic@gmail.com>
2016-12-12 16:18:48 -0600

    Update content in about and add if statements

--- app/application/controller.js
+++ app/application/controller.js
@@ -3,14 +3,4 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentUser: Ember.inject.service(),
 
-  actions: {
-    displayMenu() {
-      document.getElementsByClassName('nav-right__mobile').style.display = 'block';
-      document.getElementsByClassName('nav-right__ex').style.display = 'block';
-    },
-    closeMenu() {
-      document.getElementsByClassName('nav-right__mobile').style.display = 'none';
-      document.getElementsByClassName('nav-right__ex').style.display = 'none';
-    },
-  }
 });
