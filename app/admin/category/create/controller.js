091e20209b2b424b39d833452fb375e5850d4f60
Derrick Bray <derrickbray@yahoo.com>
2016-12-13 14:16:51 -0600

    flashMessages start

--- app/admin/category/create/controller.js
+++ app/admin/category/create/controller.js
@@ -1,20 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  flashMessages: Ember.inject.service(),
   actions: {
     addCategory(formValues) {
+      const flashMessages = Ember.get(this, 'flashMessages').add({ message: 'Custom message' });
       if (formValues) {
         const category = this.store.createRecord('category', formValues);
         category.set('category', this.model);
 
-        category.save()
-        .then(() => {
-          alert('It worked!');
+        category.save().then(() => {
+          this.transitionToRoute('admin.category');
         });
-        this.transitionToRoute('admin.category');
       } else {
         alert('It no work!');
       }
-    },
-  },
+    }
+  }
 });
