ed26d34f2609d425dbc27f6d91faf25a9adff787
Derrick Bray <derrickbray@yahoo.com>
2016-12-14 15:37:27 -0600

    remove flashMessages on user pledge Controller

--- app/user/pledge/create/controller.js
+++ app/user/pledge/create/controller.js
@@ -2,17 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
-  flashMessages: Ember.inject.service(),
+  // flashMessages: Ember.inject.service(),
   actions: {
     addPledge(formValues) {
       if (formValues) {
         const pledge = this.store.createRecord('pledge', formValues);
-        const flashMessages = this.get('flashMessages');
+        // const flashMessages = this.get('flashMessages');
         pledge.set('pledge', this.model);
 
-        pledge.save().then(() => {
-          flashMessages.success('Your Pledge Has Been Sent To The Admin.');
-        });
+        // pledge.save().then(() => {
+        //   flashMessages.success('Your Pledge Has Been Sent To The Admin.');
+        // });
         this.transitionToRoute('index');
       } else {
         alert('Sorry, Something Went Wrong. Try Again.');
